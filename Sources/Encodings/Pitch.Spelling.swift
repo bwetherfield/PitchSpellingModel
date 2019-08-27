25a777650f3c307058d8f61c253aac46f6c4dd70
Ben Wetherfield <bwetherfield@gmail.com>
2019-08-27 13:39:57 -0700

    Populate Encodings

---
+++ Sources/Encodings/Pitch.Spelling.swift
@@ -0,0 +1,52 @@
+//
+//  Pitch.Spelling.swift
+//  PitchSpeller
+//
+//  Created by Benjamin Wetherfield on 6/13/19.
+//
+
+import Pitch
+import SpelledPitch
+
+extension Pitch.Spelling {
+    
+    public static let cNatural = Pitch.Spelling(.c, .natural)
+    public static let dNatural = Pitch.Spelling(.d, .natural)
+    public static let eNatural = Pitch.Spelling(.e, .natural)
+    public static let fNatural = Pitch.Spelling(.f, .natural)
+    public static let gNatural = Pitch.Spelling(.g, .natural)
+    public static let aNatural = Pitch.Spelling(.a, .natural)
+    public static let bNatural = Pitch.Spelling(.b, .natural)
+    
+    public static let cSharp = Pitch.Spelling(.c, .sharp)
+    public static let dSharp = Pitch.Spelling(.d, .sharp)
+    public static let eSharp = Pitch.Spelling(.e, .sharp)
+    public static let fSharp = Pitch.Spelling(.f, .sharp)
+    public static let gSharp = Pitch.Spelling(.g, .sharp)
+    public static let aSharp = Pitch.Spelling(.a, .sharp)
+    public static let bSharp = Pitch.Spelling(.b, .sharp)
+
+    public static let cFlat = Pitch.Spelling(.c, .flat)
+    public static let dFlat = Pitch.Spelling(.d, .flat)
+    public static let eFlat = Pitch.Spelling(.e, .flat)
+    public static let fFlat = Pitch.Spelling(.f, .flat)
+    public static let gFlat = Pitch.Spelling(.g, .flat)
+    public static let aFlat = Pitch.Spelling(.a, .flat)
+    public static let bFlat = Pitch.Spelling(.b, .flat)
+
+    public static let cDoubleFlat = Pitch.Spelling(.c, .doubleFlat)
+    public static let dDoubleFlat = Pitch.Spelling(.d, .doubleFlat)
+    public static let eDoubleFlat = Pitch.Spelling(.e, .doubleFlat)
+    public static let fDoubleFlat = Pitch.Spelling(.f, .doubleFlat)
+    public static let gDoubleFlat = Pitch.Spelling(.g, .doubleFlat)
+    public static let aDoubleFlat = Pitch.Spelling(.a, .doubleFlat)
+    public static let bDoubleFlat = Pitch.Spelling(.b, .doubleFlat)
+
+    public static let cDoubleSharp = Pitch.Spelling(.c, .doubleSharp)
+    public static let dDoubleSharp = Pitch.Spelling(.d, .doubleSharp)
+    public static let eDoubleSharp = Pitch.Spelling(.e, .doubleSharp)
+    public static let fDoubleSharp = Pitch.Spelling(.f, .doubleSharp)
+    public static let gDoubleSharp = Pitch.Spelling(.g, .doubleSharp)
+    public static let aDoubleSharp = Pitch.Spelling(.a, .doubleSharp)
+    public static let bDoubleSharp = Pitch.Spelling(.b, .doubleSharp)
+}
