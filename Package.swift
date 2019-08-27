582ca485815eedf52013dc8336a1de4bac621592
Ben Wetherfield <bwetherfield@gmail.com>
2019-08-27 13:38:57 -0700

    Add Encodings module

--- Package.swift
+++ Package.swift
@@ -13,8 +13,15 @@ let package = Package(
         .library(
             name: "NetworkStructures",
             targets: ["NetworkStructures"]),
+        .library(
+            name: "Encodings",
+            targets: ["Encodings"]),
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/dn-m/Structure", .branch("pitchspeller-dependency")),
+        .package(url: "https://github.com/dn-m/Music", from: "0.15.0"),
+        .package(url: "https://github.com/dn-m/NotationModel", from: "0.8.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
@@ -24,6 +31,9 @@ let package = Package(
         .target(
             name: "NetworkStructures",
             dependencies: []),
+        .target(
+            name: "Encodings",
+            dependencies: ["Pitch", "SpelledPitch", "DataStructures"]),
         .testTarget(
             name: "PitchSpellingModelTests",
             dependencies: ["PitchSpellingModel"]),
