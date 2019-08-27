a651d9268da9edca07940531e0455c7f24f06c26
Ben Wetherfield <bwetherfield@gmail.com>
2019-08-27 09:41:10 -0700

    Add insertion functions

--- Sources/NetworkStructures/UnweightedNetwork.swift
+++ Sources/NetworkStructures/UnweightedNetwork.swift
@@ -8,6 +8,38 @@
 struct UnweightedNetwork<InnerNode: Hashable> {
     typealias Node = FlowNode<InnerNode>
     
-    let adjacencies: [Node: [Node]]
-    let reverseAdjacencies: [Node: [Node]]
+    private var adjacencies: [Node: [Node]] = [.source: [], .sink: []]
+    private var reverseAdjacencies: [Node: [Node]] = [.source: [], .sink: []]
+}
+
+extension UnweightedNetwork {
+    mutating func insert(_ node: InnerNode) {
+        insert(.internal(node))
+    }
+    
+    mutating func insert(_ node: Node) {
+        if !adjacencies.keys.contains(node) {
+            adjacencies[node] = []
+            reverseAdjacencies[node] = []
+        }
+    }
+    
+    mutating func edge(from start: InnerNode, to end: InnerNode) {
+        _insertEdge(from: .internal(start), to: .internal(end))
+    }
+    
+    mutating func sourceEdge(to end: InnerNode) {
+        _insertEdge(from: .source, to: .internal(end))
+    }
+    
+    mutating func sinkEdge(from start: InnerNode) {
+        _insertEdge(from: .internal(start), to: .sink)
+    }
+    
+    private mutating func _insertEdge(from start: Node, to end: Node) {
+        insert(start)
+        insert(end)
+        adjacencies[start]!.append(end)
+        reverseAdjacencies[end]!.append(start)
+    }
 }
