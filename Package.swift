30aae0ade267f49c4f6a5eb464e542f8f7e9cbb1
Ben Wetherfield <bwetherfield@gmail.com>
2019-08-27 09:23:41 -0700

    Add NetworkStructures to Package structure

--- Package.swift
+++ Package.swift
@@ -10,6 +10,9 @@ let package = Package(
         .library(
             name: "PitchSpellingModel",
             targets: ["PitchSpellingModel"]),
+        .library(
+            name: "NetworkStructures",
+            targets: ["NetworkStructures"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,6 +23,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "PitchSpellingModel",
+            dependencies: []),
+        .target(
+            name: "NetworkStructures",
             dependencies: []),
         .testTarget(
             name: "PitchSpellingModelTests",
