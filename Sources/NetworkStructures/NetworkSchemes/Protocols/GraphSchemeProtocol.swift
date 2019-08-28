cc87fc91b6154e1b930fcc91c72e317c8bb2f7a3
Ben Wetherfield <bwetherfield@gmail.com>
2019-08-28 08:42:24 -0700

    Remove adjacencyScheme conversion

--- Sources/NetworkStructures/NetworkSchemes/Protocols/GraphSchemeProtocol.swift
+++ Sources/NetworkStructures/NetworkSchemes/Protocols/GraphSchemeProtocol.swift
@@ -11,13 +11,3 @@ public protocol GraphSchemeProtocol {
     associatedtype Node
     associatedtype Edge: SymmetricPair where Edge.A == Node
 }
-
-extension GraphProtocol {
-    func adjacencyScheme <G> () -> G where
-        G: UnweightedGraphSchemeProtocol,
-        G.Edge == Edge,
-        G.Node == Node
-    {
-        return G(self.contains)
-    }
-}
