537cfece05f0ec781142ec0599dd10fe642f55c8
Ben Wetherfield <bwetherfield@gmail.com>
2019-09-26 14:16:21 -0700

    Rewrite UnassignedInnerNode as a typealias

--- Sources/SpellingNetworks/UnassignedNode.swift
+++ Sources/SpellingNetworks/UnassignedNode.swift
@@ -10,7 +10,13 @@ import NetworkStructures
 import DataStructures
 
 typealias UnassignedNode = FlowNode<UnassignedInnerNode>
+typealias UnassignedInnerNode = Cross<Int, Tendency>
 
-struct UnassignedInnerNode: Hashable {
-    let index: Cross<Int, Tendency>
+extension Cross where A == Int, B == Tendency {
+
+    init (index: Cross<Int, Tendency>) {
+        self = index
+    }
+
+    var index: Cross<Int, Tendency> { return self }
 }
