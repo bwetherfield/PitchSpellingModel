e28dbec96dcb5abf9544d9d2600aac5ca3e703c4
Ben Wetherfield <bwetherfield@gmail.com>
2019-09-26 14:47:00 -0700

    Rename schemes to differentInt and sameInt

--- Sources/SpellingNetworks/PitchSpellingNetwork.swift
+++ Sources/SpellingNetworks/PitchSpellingNetwork.swift
@@ -30,12 +30,12 @@ extension PitchSpellingNetwork {
             list.append(.init(int, .down))
             list.append(.init(int, .up))
         }
-        let betweenScheme: FlowNetworkScheme<Cross<Int, Tendency>> = weightScheme.pullback { cross in
+        let differentIntScheme: FlowNetworkScheme<Cross<Int, Tendency>> = weightScheme.pullback { cross in
             let pitchClass = pitch(cross.a).class
             return Cross(pitchClass, cross.b)
             }
-        let withinScheme: FlowNetworkScheme<Cross<Int, Tendency>> = Double.infinity * (Connect.sameInts * Connect.upToDown)
-        let combinedScheme: FlowNetworkScheme<Cross<Int, Tendency>> = withinScheme + betweenScheme
+        let sameIntScheme: FlowNetworkScheme<Cross<Int, Tendency>> = Double.infinity * (Connect.sameInts * Connect.upToDown)
+        let combinedScheme: FlowNetworkScheme<Cross<Int, Tendency>> = sameIntScheme + differentIntScheme
         self.flowNetwork = FlowNetwork(
             nodes: nodes,
             scheme: combinedScheme
