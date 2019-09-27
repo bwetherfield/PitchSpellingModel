fb67d0e6ae0defb2540c6121323344d2727c67ce
Ben Wetherfield <bwetherfield@gmail.com>
2019-09-27 15:11:39 -0700

    Add edge test

---
+++ Tests/NetworkStructuresTests/FlowNetworkTests.swift
@@ -0,0 +1,25 @@
+//
+//  FlowNetworkTests.swift
+//  NetworkStructuresTests
+//
+//  Created by Benjamin Wetherfield on 9/27/19.
+//
+
+import XCTest
+@testable import NetworkStructures
+
+class FlowNetworkTests: XCTestCase {
+    
+    func testEdges() {
+        var network = FlowNetwork<String>()
+        network.insert("a")
+        network.insert("b")
+        network.edge(from: "a", to: "b", withWeight: 2)
+        network.sourceEdge(to: "a", withWeight: 1)
+        network.sinkEdge(from: "b", withWeight: 1)
+        XCTAssertEqual(network.neighbors(of: .internal("b")), [.sink])
+        XCTAssertEqual(network.neighbors(of: .internal("a")), [.internal("b")])
+        XCTAssertEqual(network.neighbors(of: .source), [.internal("a")])
+        XCTAssertEqual(network.neighbors(of: .sink), [])
+    }
+}
