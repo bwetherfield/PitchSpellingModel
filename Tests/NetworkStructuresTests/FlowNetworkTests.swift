6080658b6097ea8b05f682f549600f5987d75af7
Ben Wetherfield <bwetherfield@gmail.com>
2019-09-27 15:24:37 -0700

    Factor out setup and teardown

--- Tests/NetworkStructuresTests/FlowNetworkTests.swift
+++ Tests/NetworkStructuresTests/FlowNetworkTests.swift
@@ -10,13 +10,18 @@ import XCTest
 
 class FlowNetworkTests: XCTestCase {
     
-    func testEdges() {
-        var network = FlowNetwork<String>()
+    var network: FlowNetwork<String>!
+    
+    override func setUp() {
+        network = FlowNetwork<String>()
         network.insert("a")
         network.insert("b")
         network.edge(from: "a", to: "b", withWeight: 2)
         network.sourceEdge(to: "a", withWeight: 1)
         network.sinkEdge(from: "b", withWeight: 1)
+    }
+    
+    func testEdges() {
         XCTAssertEqual(network.neighbors(of: .internal("b")), [.sink])
         XCTAssertEqual(network.neighbors(of: .internal("a")), [.internal("b")])
         XCTAssertEqual(network.neighbors(of: .source), [.internal("a")])
@@ -24,14 +29,12 @@ class FlowNetworkTests: XCTestCase {
     }
     
     func testRemoveEdges() {
-        var network = FlowNetwork<String>()
-        network.insert("a")
-        network.insert("b")
-        network.edge(from: "a", to: "b", withWeight: 2)
-        network.sourceEdge(to: "a", withWeight: 1)
-        network.sinkEdge(from: "b", withWeight: 1)
         network.removeEdge(from: .internal("a"), to: .internal("b"))
         XCTAssertEqual(network.neighbors(of: .internal("a")), [])
         XCTAssertEqual(network.reverseNeighbors(of: .internal("b")), [])
+    }
+    
+    override func tearDown() {
+        network = nil
     }
 }
