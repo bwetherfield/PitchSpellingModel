68c33fe342a65c689c34915d87159a0fe6e6ca4b
Ben Wetherfield <bwetherfield@gmail.com>
2019-12-30 08:59:59 -0800

    Add explicit PitchSpellingNetwork Factory class

--- Sources/SpellingNetworks/PitchSpellingNetworkFactory.swift
+++ Sources/SpellingNetworks/PitchSpellingNetworkFactory.swift
@@ -5,4 +5,28 @@
 //  Created by Benjamin Wetherfield on 12/30/19.
 //
 
-import Foundation
+import Pitch
+import DataStructures
+import NetworkStructures
+import Encodings
+
+class PitchSpellingNetworkFactory {
+    
+    let weightScheme: FlowNetworkScheme<Cross<Pitch.Class, Tendency>>
+    
+    init (_ weightScheme: FlowNetworkScheme<Cross<Pitch.Class, Tendency>>) {
+        self.weightScheme = weightScheme
+    }
+    
+    func build (from pitches: [Int: Pitch]) -> PitchSpellingNetwork {
+        return PitchSpellingNetwork(pitches: pitches, weightScheme: weightScheme)
+    }
+    
+    func build (from pitches: [[Pitch]]) -> PitchSpellingNetwork {
+        return PitchSpellingNetwork(pitches: pitches, weightScheme: weightScheme)
+    }
+    
+    func build (from pitches: [Pitch]) -> PitchSpellingNetwork {
+        return PitchSpellingNetwork(pitches: pitches, weightScheme: weightScheme)
+    }
+}
